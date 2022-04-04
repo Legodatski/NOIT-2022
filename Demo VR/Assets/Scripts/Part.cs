d5ec1277ba240182e587da6e9f9d693d5e1a9cc1
Legodatski <ovinedev@abv.bg>
2022-04-04 14:17:44 +0300

    updated building ak47

--- Demo VR/Assets/Scripts/Part.cs
+++ Demo VR/Assets/Scripts/Part.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Part : MonoBehaviour
 {
     public bool IsConnected = false;
+    bool CorrectPlace = false;
+
+    public void SetCorrectPlaceTrue() => CorrectPlace = true;
+    public void SetCorrectPlaceFalse() => CorrectPlace = false;
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Ak47")
+        if (collision.gameObject.tag == "Ak47" && CorrectPlace)
         {
             Debug.Log("Connected - " + gameObject.name);
             IsConnected = true;
@@ -17,18 +21,18 @@ public class Part : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Ak47")
+        if (other.gameObject.tag == "Ak47" && CorrectPlace)
         {
             IsConnected = true;
         }
     }
 
     private void OnTriggerExit(Collider collision)
-    {
-        if (collision.gameObject.tag == "Ak47")
+    { 
+        if (collision.gameObject.tag == "Ak47" && CorrectPlace)
         {
             Debug.Log("Disonnected - " + gameObject.name);
-            IsConnected = false;
+            //IsConnected = false;
         }
     }
 }
