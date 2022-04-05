f78645314ff559b2f2a78bed0fbe58be5148d1b0
davidpetkov <davidpetkov2006@gmail.com>
2022-04-05 19:29:19 +0300

    Fixed material.

--- Demo VR/Assets/Scripts/RayRight.cs
+++ Demo VR/Assets/Scripts/RayRight.cs
@@ -6,6 +6,6 @@ public class RayRight : MonoBehaviour
 {
     public Material ray;
 
-    public void DisableRay() => ray.color = Color.clear;
+    public void DisableRay() => ray.color = new Color(0.5f, 0.5f, 0.5f, 0f);
     public void EnableRay() => ray.color = Color.white;
 }
